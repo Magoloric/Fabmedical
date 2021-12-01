f1573e5b0b176ba880e19ce4f7fd0dae77c32393
Valery Lazarev <valery.lazarev@regionvarmland.se>
2021-12-01 11:41:04 +0000

    Setup Environment Variables

--- content-web/app.js
+++ content-web/app.js
@@ -6,7 +6,7 @@ const request = require('request');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
-const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
+const contentApiUrl = process.env.CONTENT_API_URL || "http://20.120.28.232:3001";
 
 
 function getSessions(cb) {
